4e2a1d3f767051d99104812ebcec9af5d588dd75
UmbraSpirits <eternalmillenis@outlook.com>
2020-07-31 14:01:16 +0200

    added scatterplot with 2 linear regression lines

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,23 +1,33 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.stats import linregress
+
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv("epa-sea-level.csv")
 
     # Create scatter plot
-
-
+    plt.scatter(df["Year"], df["CSIRO Adjusted Sea Level"])
     # Create first line of best fit
+    slope, intercept, r_value, p_value, std_err = linregress(
+        df["Year"], df["CSIRO Adjusted Sea Level"])
 
+    x_extended = np.arange(1880, 2050)
 
+    plt.plot(x_extended, intercept + slope*x_extended, "r")
     # Create second line of best fit
+    dfNew = df.loc[(df["Year"] >= 2000)]
 
+    slopeNew, interceptNew, r_valueNew, p_valueNew, std_errNew = linregress(
+        dfNew["Year"], dfNew["CSIRO Adjusted Sea Level"])
 
+    plt.plot(x_extended, interceptNew + slopeNew*x_extended, "r")
     # Add labels and title
-
-    
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
+    plt.title("Rise in Sea Level")
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
